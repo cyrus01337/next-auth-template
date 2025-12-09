5c4de1e35902613be53a8a336436c6c8b52694d5
cyrus01337 <cyrus01337@gmail.com>
2025-12-09 12:48:20 +0000

    Add barebones "dashboard" page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+"use server";
+
+export default async function Dashboard() {
+    return (
+        <div className="grow box-border p-6.5">
+            <span>Dashboard</span>
+        </div>
+    );
+}
