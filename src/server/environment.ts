7f26fe591f3a5ebe3c1b0adfadb61e272da4aa2d
cyrus01337 <cyrus01337@gmail.com>
2025-12-09 12:44:44 +0000

    Simplify global project name

--- src/server/environment.ts
+++ src/server/environment.ts
@@ -4,12 +4,14 @@ import { createEnv } from "@t3-oss/env-core";
 import { z as zod } from "zod";
 
 export default createEnv({
-    client: {},
+    client: {
+        NEXT_PUBLIC_PROJECT_NAME: zod.string().optional().default("Project Name"),
+    },
     clientPrefix: "NEXT_PUBLIC_",
     emptyStringAsUndefined: true,
     runtimeEnv: process.env,
     server: {
+        NEXTAUTH_SECRET: zod.string().nonempty(),
         NEXTAUTH_URL: zod.url(),
-        NEXTAUTH_SECRET: zod.string().min(1),
     },
 });
